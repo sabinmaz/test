1e2b34f4610dafa9031deb61479e5285c2a1dc1d
sabin <sbinmaharjan@gmail.com>
2017-09-04 00:28:40 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "208.91.198.109";
 $username = "itglarid_docsea";
-$password = "docsea%23";
+$password = "doc@sea12";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
