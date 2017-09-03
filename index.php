5cb98b1043b0dc89c685d3ed8ea1b58a10f1aa94
sabin <sbinmaharjan@gmail.com>
2017-09-04 00:20:57 +0545

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,10 @@ $servername = "208.91.198.109";
 $username = "itglarid_docsea";
 $password = "docsea%23";
 
-// Create connection
-$conn = new mysql($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$link = mysql_connect($servername, $username, $password);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
 ?>
