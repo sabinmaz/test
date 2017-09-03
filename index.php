68accf75427d6e9e57b67bac97c539aaca27defd
sabin <sbinmaharjan@gmail.com>
2017-09-03 23:18:31 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "208.91.198.109";
+$username = "itglarid_docsea";
+$password = "doc@sea12";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
